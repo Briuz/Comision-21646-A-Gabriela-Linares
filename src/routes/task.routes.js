79fdc51aea0d28463ea8f86b121529954c1fb750
Briuz <missbriuz@gmail.com>
2023-09-13 17:53:01 -0300

    actualizacion de rutas, el error 404 fue corregido

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import { ctrlCreateTasks, ctrlDeleteTasks, ctrlGetTasks, ctrlUpdateTasks } from "../controllers/task.controllers.js";
+import { validator} from "../middlewares/validator.js";
+import { TaskModel } from "../models/Tasks.js";
 
 const taskRouter = Router();
 
 //endpoint para traer todas las tareas
-taskRouter.get('/api/tasks', ctrlGetTasks)
+taskRouter.get('/api/tasks', ctrlGetTasks);
 
 //endpoint para crear una tareas
-taskRouter.post('/api/tasks', ctrlCreateTasks)
+taskRouter.post('/api/tasks', ctrlCreateTasks);
 
 //endpoint para modificar una tareas
-taskRouter.put('/api/:id', ctrlUpdateTasks)
+taskRouter.put('/api/tasks/:id', ctrlUpdateTasks);
 
 //endpoint para eliminar una tareas
-taskRouter.delete('/api/:id', ctrlDeleteTasks)
+taskRouter.delete('/api/tasks/:id', ctrlDeleteTasks);
 
-export { taskRouter }
+export { taskRouter };
